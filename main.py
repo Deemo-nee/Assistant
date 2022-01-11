8d952c877867da2a32834a48ad2f0dd3d911c08b
Phan_Cuong <cuongcotcp@gmail.com>
2022-01-11 16:50:20 +0700

    update

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import kivy
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.core.window import Window
+
+Window.size = (350, 580)
+
 
 class MainApp(MDApp):
     def build(self):
         self.title = 'Home Assistant'
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
-
 
 
 MainApp().run()
